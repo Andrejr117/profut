7628959cacd7e9eec5082edd95f94cb8474ace26
Matheus <matheusfelip4004@gmail.com>
2023-09-28 00:55:23 -0300

    Tela de perfil funcionando, mas,  não tem nada.

---
+++ src/app/perfil/page.jsx
@@ -0,0 +1,14 @@
+// pages/profile.js
+import React from 'react';
+
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>Meu Perfil</h1>
+      
+    </div>
+  );
+};
+
+export default ProfilePage;
