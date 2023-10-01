78c52b4311e37828d1f6c48001a7384b2286ad33
Matheus <matheusfelip4004@gmail.com>
2023-10-01 00:32:19 -0300

    .

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -1,11 +1,11 @@
 // pages/profile.js
 import React from 'react';
-
+import style from '../perfil/styles.module.css'
 
 const ProfilePage = () => {
   return (
     <div>
-      <h1>Meu Perfil</h1>
+      <h1 className={style.ProfilePage}>Meu Perfil</h1>
       
     </div>
   );
