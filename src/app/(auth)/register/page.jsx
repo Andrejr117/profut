666269bd8de29fbb4a5d554e2d569de9a1b49967
Andrejr117 <andre_jr97@hotmail.com>
2023-10-01 23:37:02 -0300

    cadastro 0.1

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -4,11 +4,16 @@ import Button from "@/components/Button";
 import styles from './styles.module.css';
 import Link from "next/link";
 import { Form, Formik } from "formik";
-import React from 'react'
+import React, { useState } from 'react'
 import * as Yup from "yup";
+import { useRouter } from "next/navigation";
 
 
 export default function Register() {
+  const [error, setError] = useState("");
+  const [isFormSubmitting, setFormSubmitting] = useState(false);
+
+  const router = useRouter();
   const initialValues = {
     nome: "",
     email: "",
@@ -27,23 +32,62 @@ export default function Register() {
     posição: Yup.string().required("O campo posição é obrigatório"),
   });
 
-  async function handleSubmit(values) {
-    console.log(values);
+  async function handleSubmit(values, { resetForm }) {
+    setFormSubmitting(true);
+    try {
+      await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.Stringify({
+          nome: values.nome,
+          email: values.email,
+          senha: values.senha,
+          nacionalidade: values.nacionalidade,
+          altura: values.altura,
+          posição: values.posição,
+        }),
+      }).then(async (res) => {
+        const result = await res.json()
+
+        if (result.status === 201) {
+          alert(result.message);
+          router.push("/login");
+        } else {
+          renderError(result.message);
+          resetForm();
+        }
+
+        setFormSubmitting(false);
+      });
+    } catch (error) {
+      setFormSubmitting(false);
+      renderError("Erro ao criar conta, tente novamente mais tarde !");
+
+    }
+  }
+
+  function renderError(msg) {
+    setError(msg);
+    setTimeout(() => {
+      setError("");
+    }, 3000);
   }
 
   return (
     <main className="min-h-screen flex items-center justify-center flex-col">
       <h1 className={styles.textForm}>Crie sua conta e fique pronto para marcar</h1>
 
-      <Formik 
-        initialValues={initialValues} 
+      <Formik
+        initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
         {({ values }) => (
-          <Form 
-          noValidate 
-          className="flex flex-col gap-2 p-4 border rounded border-zinc-300 min-w-[300px] bg-white">
+          <Form
+            noValidate
+            className="flex flex-col gap-2 p-4 border rounded border-zinc-300 min-w-[300px] bg-white">
             <Input
               name="nome"
               type="text"
@@ -87,7 +131,7 @@ export default function Register() {
               className="bg-green-500 text-white rounded p-2 cursor-pointer"
             />
             <span className="text-xs text-zinc-500">
-              Já possui uma conta? 
+              Já possui uma conta?
               <strong className="text-zinc-700">
                 <Link href="/login"> Clique aqui para logar</Link>
               </strong>
--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -2,3 +2,45 @@ import User from "@/models/User";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import connect from "@/utils/db";
+
+export async function POST(req) {
+    try {
+        const { nome, email, senha, nacionalidade, altura, posição } = await req.json();
+        await connect();
+
+        const emailExist = await User.findOne({ email });
+
+        if (emailExist) {
+            return NextResponse.json({
+                message: "E-mail já cadastrado !",
+                status: 409,
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(senha, 5);
+
+        const newUser = new User({
+            nome,
+            email,
+            senha: hashedPassword,
+            nacionalidade,
+            altura,
+            posição,
+        });
+
+        await newUser.save();
+
+        return NextResponse.json({
+            message: "Usuário criado com sucesso !",
+            status: 201,
+        });
+
+    } catch (error) {
+        return NextResponse.json({
+            error: "Erro ao cadastrar usuário",
+            status: 500,
+        });
+
+    }
+}
+
